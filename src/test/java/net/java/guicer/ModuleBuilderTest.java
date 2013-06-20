abf5046a3faff4172b8ee54cafce9bd03768ab46
Christian Schlichtherle <christian@schlichtherle.de>
2013-06-20 11:50:15 +0200

    Added <inceptionYear>.

--- src/test/java/net/java/guicer/ModuleBuilderTest.java
+++ src/test/java/net/java/guicer/ModuleBuilderTest.java
@@ -4,42 +4,45 @@
  */
 package net.java.guicer;
 
-import com.google.inject.Module;
-import com.google.inject.PrivateModule;
+import com.google.inject.Injector;
 import static com.google.inject.name.Names.named;
-import javax.inject.Singleton;
+import javax.inject.*;
+import org.junit.*;
 import static org.junit.Assert.*;
-import org.junit.Ignore;
-import org.junit.Test;
 
 /**
- *
  * @author Christian Schlichtherle
  */
 public class ModuleBuilderTest {
 
-    @Ignore("Module.equals() is not defined.")
     @Test
     public void testConfiguration() {
-        final Module guice = new PrivateModule() {
-            @Override protected void configure() {
-                expose(Object.class)
-                        .annotatedWith(named("object"));
-                bind(Object.class)
-                        .annotatedWith(named("object"))
-                        .to(Object.class)
-                        .in(Singleton.class);
-            }
-        };
-        final Module guicer = new GuiceContext()
+        final Injector injector = new GuiceContext()
                 .injector()
                     .module()
-                        .exposeAndBind(Object.class)
-                            .to(Object.class)
-                            .annotatedWith(named("object"))
+                        .bind(Bar.class)
+                            .to(BarImpl.class)
                             .in(Singleton.class)
                             .inject()
-                        .build();
-        assertEquals(guice, guicer);
+                        .bind(Foo.class)
+                            .annotatedWith(named("foo"))
+                            .to(FooImpl.class)
+                            .inject()
+                        .inject()
+                    .build();
+        final Bar bar1 = injector.getInstance(Bar.class);
+        final Bar bar2 = injector.getInstance(Bar.class);
+        assertSame(bar1, bar2);
+    }
+
+    private interface Foo { }
+
+    private static class FooImpl implements Foo { }
+
+    private interface Bar { }
+
+    //@Singleton
+    private static class BarImpl implements Bar {
+        @Inject BarImpl(@Named("foo") Foo foo) { }
     }
 }
